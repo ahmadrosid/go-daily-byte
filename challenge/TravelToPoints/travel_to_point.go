e2ca661db0a6ee37c579d0e24b1a6b4d4286d7b4
ahmadrosid <alahmadrosid@gmail.com>
2021-10-08 21:21:24 +0700

    feat: update readme

--- challenge/TravelToPoints/travel_to_point.go
+++ challenge/TravelToPoints/travel_to_point.go
@@ -1,1 +1,13 @@
 package traveltopoints
+
+func Solve(points [][]int) int {
+	lenX, lenY := len(points[0]), len(points)
+	matrix := make([][]int, lenX)
+
+	for x := 1; x < lenX; x++ {
+		matrix[x] = make([]int, lenY)
+		matrix[x][0] = points[x][0]
+	}
+
+	return 0
+}
--- challenge/TravelToPoints/travel_to_point_test.go
+++ challenge/TravelToPoints/travel_to_point_test.go
@@ -1,1 +1,25 @@
 package traveltopoints
+
+import "testing"
+
+func Test_Solve(t *testing.T) {
+	point1 := [][]int{
+		{0, 0}, {1, 1}, {2, 2},
+	}
+	point2 := [][]int{
+		{0, 1}, {2, 3}, {4, 0},
+	}
+	tests := []struct {
+		Paths    [][]int
+		Expected int
+	}{
+		{point1, 2},
+		{point2, 5},
+	}
+	for _, data := range tests {
+		res := Solve(data.Paths)
+		if res != data.Expected {
+			t.Fatalf("expected: '%d' got: '%d'", data.Expected, res)
+		}
+	}
+}
